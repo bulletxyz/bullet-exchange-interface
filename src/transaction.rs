7e1b819e4d9a03b71261012c4dcb0a203995d3fc
Bernhard Kauer <bernhard@sierra.team>
2026-07-07 15:36:58 +0200

    fix: adopt to changed coins definition

--- src/transaction.rs
+++ src/transaction.rs
@@ -449,9 +449,25 @@ pub mod bank {
     define_struct! {
         #[cfg_attr(feature="schema", sov_wallet(show_as = "{} coins of token ID {}"))]
         struct Coins {
-            #[cfg_attr(feature="schema", sov_wallet(fixed_point(from_field(1, offset = 31))))]
+            #[cfg_attr(feature="schema",
+               sov_wallet(fixed_point(from_field(1, offset = 31,
+                  override_eq = config_gas_token_id().0,
+                  override_decimals = config_gas_token_decimals()))))
+        ]
             amount: Amount,
             token_id: TokenId,
         }
+    }
+
+    pub fn config_gas_token_id() -> TokenId {
+        TokenId([
+            153, 62, 252, 188, 142, 200, 250, 132, 195, 171, 34, 22, 170, 139, 109, 60, 134, 53,
+            48, 11, 99, 82, 21, 138, 87, 53, 145, 193, 40, 198, 52, 89,
+        ])
+    }
+
+    /// The number of decimal places of the rollup's gas token.
+    pub fn config_gas_token_decimals() -> u8 {
+        9
     }
 }
